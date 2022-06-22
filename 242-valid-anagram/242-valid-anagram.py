592776c541e45603291eb953ccbd3280a45efde6
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-06-22 20:58:25 +0530

    Time: 120 ms (7.24%), Space: 14.4 MB (96.95%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,19 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        word_dict = {}
+        word_dict = [0 for i in range(26)]
         for i in s:
-            if i in word_dict.keys():
-                word_dict[i] = word_dict[i] + 1
-            else:
-                word_dict[i] = 1
+            word_dict[ord(i) - ord('a')] = word_dict[ord(i) - ord('a')] + 1
+           
                 
         for j in t:
-            if j not in word_dict.keys():
-                return False
-            else:
-                word_dict[j] = word_dict[j] - 1
-                
-        return (not any(word_dict.values()))
+            word_dict[ord(j) - ord('a')] = word_dict[ord(j) - ord('a')] - 1
+        print(word_dict)   
+        print(not any(word_dict))
+        return not any(word_dict)
     
         
         
