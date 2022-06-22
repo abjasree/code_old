4c90f1b4b3c40f6a8d000f8b29f8b9570826e812
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-06-22 20:45:08 +0530

    Time: 122 ms (6.85%), Space: 14.5 MB (66.36%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        word_dict = {}
+        for i in s:
+            if i in word_dict.keys():
+                word_dict[i] = word_dict[i] + 1
+            else:
+                word_dict[i] = 1
+                
+        for j in t:
+            if j not in word_dict.keys():
+                return False
+            else:
+                word_dict[j] = word_dict[j] - 1
+                
+        return (not any(word_dict.values()))
+    
+        
+        
+    def brute_force_isAnagram(self, s: str, t: str) -> bool:
         s = sorted(s)
         t = sorted(t)
         return s == t
-        
