e54dac47267e9e2cd6685ace365872f1bcdc966c
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-06-29 23:05:35 +0530

    Time: 70 ms (5.14%), Space: 13.8 MB (60.65%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -5,7 +5,7 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left = 1
         right = n
-        
+      
         while left < right:
             mid = left + (right - left) // 2
             if isBadVersion(mid):
@@ -14,7 +14,9 @@ class Solution:
                 left = mid+1
         return left
                 
-                
+        
+            
+        
         
     def brute_force_firstBadVersion(self, n: int) -> int:
         for i in range(1, n+1):
