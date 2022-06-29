241a07dd5bea27a03f90106533c55c53ca3fa0e1
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-06-29 23:04:20 +0530

    Time: 59 ms (14.41%), Space: 13.8 MB (60.65%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,23 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
+                
+                
+        
+    def brute_force_firstBadVersion(self, n: int) -> int:
+        for i in range(1, n+1):
+            if isBadVersion(i) == True:
+                return i
+        
