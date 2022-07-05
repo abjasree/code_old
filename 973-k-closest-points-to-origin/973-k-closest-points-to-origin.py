b28f5c16a45313e007755bff2472045d8a8500ae
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-05 20:45:19 +0530

    Time: 1393 ms (27.37%), Space: 20.6 MB (20.67%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -3,11 +3,11 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
         for x,y in points:
-            dist = - sqrt(x*x + y*y)
+            dist = sqrt(x*x + y*y)
             if len(heap) == k:
-                heapq.heappushpop(heap, (dist, x, y))
+                heapq.heappushpop(heap, (-dist, x, y))
             else:
-                heapq.heappush(heap, (dist, x, y))
+                heapq.heappush(heap, (-dist, x, y))
         return [[x,y] for (dist, x, y) in heap]
     
     def brute_force_kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
