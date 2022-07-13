b6c4f8a03066195697661f94cc7bb6410ad2f860
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-13 23:36:38 +0530

    Time: 81 ms (9.70%), Space: 14.4 MB (26.20%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -10,6 +10,20 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return
         dict_nodes = {node: Node(node.val)}
+        stack = [node]
+        while stack:
+            curr_node = stack.pop()
+            for neigh in curr_node.neighbors:
+                if neigh not in dict_nodes:
+                    dict_nodes[neigh] = Node(neigh.val)
+                    stack.append(neigh)
+                dict_nodes[curr_node].neighbors.append(dict_nodes[neigh])
+                
+        return dict_nodes[node]
+    
+    def bfs_cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return
+        dict_nodes = {node: Node(node.val)}
         queue = deque([node])
         while queue:
             for i in range(len(queue)):
