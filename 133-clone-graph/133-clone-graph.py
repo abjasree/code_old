e6f55bd9a5a1924e833f00c6e231e2bdcbfb09ad
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-13 23:37:56 +0530

    Time: 60 ms (47.51%), Space: 14.4 MB (26.20%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -10,6 +10,21 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return
         dict_nodes = {node: Node(node.val)}
+        queue = [node]
+        while queue:
+            for i in range(len(queue)):
+                curr_node = queue.pop(0)
+                for neigh in curr_node.neighbors:
+                    if neigh not in dict_nodes:
+                        dict_nodes[neigh] = Node(neigh.val)
+                        queue.append(neigh)
+                    dict_nodes[curr_node].neighbors.append(dict_nodes[neigh])
+                    
+        return dict_nodes[node]
+    
+    def dfs_cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return
+        dict_nodes = {node: Node(node.val)}
         stack = [node]
         while stack:
             curr_node = stack.pop()
@@ -21,17 +36,4 @@ class Solution:
                 
         return dict_nodes[node]
     
-    def bfs_cloneGraph(self, node: 'Node') -> 'Node':
-        if not node: return
-        dict_nodes = {node: Node(node.val)}
-        queue = deque([node])
-        while queue:
-            for i in range(len(queue)):
-                curr_node = queue.popleft()
-                for neigh in curr_node.neighbors:
-                    if neigh not in dict_nodes:
-                        dict_nodes[neigh] = Node(neigh.val)
-                        queue.append(neigh)
-                    dict_nodes[curr_node].neighbors.append(dict_nodes[neigh])
-                    
-        return dict_nodes[node]
+    
